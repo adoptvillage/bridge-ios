68d88e0e78ef2dc149541cabe59944daf258feb0
Abhi Kulshrestha <abhikulshrestha1@gmail.com>
2020-09-08 23:24:09 +0530

    Initial setup
    Login/Signup screen

---
+++ BRIDGE/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  BRIDGE
+//
+//  Created by Abhi Kulshrestha on 28/08/20.
+//  Copyright © 2020 Abhi Kulshrestha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      //  Text("Hello, World!")
+        ZStack{
+        
+            main()
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+
